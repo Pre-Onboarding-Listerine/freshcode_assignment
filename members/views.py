321a07270e2f0feb7583abf6b841620fba6d5aaa
blingblin-g <luvbliny@gmail.com>
2021-11-06 04:34:18 +0900

    REFACTOR : status code 변경

--- members/views.py
+++ members/views.py
@@ -3,7 +3,6 @@ from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.contrib.auth.hashers import check_password
 import jwt
 from .models import User
 from .serializers import UserSerializer
@@ -20,8 +19,8 @@ class SigninView(APIView):
 	def post(self, request, format=None):
 		user = User.objects.get(email=request.data["email"])
 		if not user:
-			return Response(" 존재하지 않는 이메일 ", status=status.HTTP_400_BAD_REQUEST)
+			return Response(" 존재하지 않는 이메일 ", status=status.HTTP_404_NOT_FOUND)
 		if not bcrypt.checkpw(request.data["password"].encode('utf-8'), user.password.encode('utf-8')):
-			return Response(" 유효하지 않은 비밀번호 ", status=status.HTTP_400_BAD_REQUEST)
+			return Response(" 유효하지 않은 비밀번호 ", status=status.HTTP_401_UNAUTHORIZED)
 		token = jwt.encode({'id': user.id }, SECRET)
 		return Response(token, status=status.HTTP_200_OK)
